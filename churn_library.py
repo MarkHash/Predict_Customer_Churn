5128d6a41f7792ac59ca80387615f611c7717141
MarkHash <makoto.hashimoto06@gmail.com>
2022-11-23 11:47:31 +0900

    followed pylint

--- churn_library.py
+++ churn_library.py
@@ -8,19 +8,19 @@ Date: Nov 2022
 
 # import libraries
 import os
+from sklearn.metrics import plot_roc_curve, classification_report
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 import joblib
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns; sns.set()
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import plot_roc_curve, classification_report
+import seaborn as sns
+sns.set()
 
-os.environ['QT_QPA_PLATFORM']='offscreen'
-
+os.environ['QT_QPA_PLATFORM'] = 'offscreen'
 
 
 def import_data(pth):
@@ -35,73 +35,74 @@ def import_data(pth):
     return pd.read_csv(pth)
 
 
-def perform_eda(df):
+def perform_eda(dataframe):
     '''
-    perform eda on df and save figures to images folder
+    perform eda on dataframe and save figures to images folder
     input:
-            df: pandas dataframe
+            dataframe: pandas dataframe
 
     output:
             None
     '''
-    df['Churn'] = df['Attrition_Flag'].apply(lambda val: 0 if val == "Existing Customer" else 1)
-    
+    dataframe['Churn'] = dataframe['Attrition_Flag'].apply(
+        lambda val: 0 if val == "Existing Customer" else 1)
+
     # histgram of churn
-    df['Churn'].hist()
+    dataframe['Churn'].hist()
     plt.savefig('./images/eda/churn_hist.png')
     plt.close('churn_hist.png')
 
     # histogram of Customer_Age
-    df['Customer_Age'].hist()
+    dataframe['Customer_Age'].hist()
     plt.savefig('./images/eda/Customer_Age.png')
     plt.close('Customer_Age.png')
 
     # histogram of Marital_Status
-    df['Marital_Status'].value_counts('normalize').plot(kind='bar')
+    dataframe['Marital_Status'].value_counts('normalize').plot(kind='bar')
     plt.savefig('./images/eda/Marital_Status.png')
     plt.close('Marital_Status.png')
 
     # histogram of Total_Trans_Ct
-    sns.histplot(df['Total_Trans_Ct'], stat='density', kde=True)
+    sns.histplot(dataframe['Total_Trans_Ct'], stat='density', kde=True)
     plt.savefig('./images/eda/Total_Trans_Ct.png')
     plt.close('Total_Trans_Ct.png')
 
     # heatmap
-    sns.heatmap(df.corr(), annot=False, cmap='Dark2_r', linewidths = 2)
+    sns.heatmap(dataframe.corr(), annot=False, cmap='Dark2_r', linewidths=2)
     plt.savefig('./images/eda/heatmap.png')
     plt.close('heatmap.png')
 
-def encoder_helper(df, category_lst, response):
+
+def encoder_helper(dataframe, category_lst, response):
     '''
     helper function to turn each categorical column into a new column with
     propotion of churn for each category - associated with cell 15 from the notebook
 
     input:
-            df: pandas dataframe
+            dataframe: pandas dataframe
             category_lst: list of columns that contain categorical features
-            response: string of response name [optional argument that could be used for naming variables or index y column]
+            response: string of response name
 
     output:
-            df: pandas dataframe with new columns for
+            dataframe: pandas dataframe with new columns for
     '''
     for category_name in category_lst:
         lst = []
         column_name = category_name + response
-        category_groups = df.groupby(category_name).mean()['Churn']
-        for val in df[category_name]:
+        category_groups = dataframe.groupby(category_name).mean()['Churn']
+        for val in dataframe[category_name]:
             lst.append(category_groups.loc[val])
 
-        df[column_name] = lst
-    return df
+        dataframe[column_name] = lst
+    return dataframe
 
 
-
-def perform_feature_engineering(df, cols, response):
+def perform_feature_engineering(dataframe, cols, response):
     '''
     input:
-              df: pandas dataframe
+              dataframe: pandas dataframe
               cols: list of columns that will be kept for model training
-              response: string of response name [optional argument that could be used for naming variables or index y column]
+              response: string of response name
 
     output:
               X_train: X training data
@@ -109,14 +110,15 @@ def perform_feature_engineering(df, cols, response):
               y_train: y training data
               y_test: y testing data
     '''
-    X = pd.DataFrame()
-    X[cols] = df[cols]
-    y = df[response]
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+    X_dataframe = pd.DataFrame()
+    X_dataframe[cols] = dataframe[cols]
+    y_dataframe = dataframe[response]
+    X_train, X_test, y_train, y_test = train_test_split(
+        X_dataframe, y_dataframe, test_size=0.3, random_state=42)
     return X_train, X_test, y_train, y_test
 
 
-def classification_report_image(y_train, y_test, y_train_preds, y_test_preds, model_name, output_pth):
+def classification_report_image(y_train, y_test, y_train_preds, y_test_preds, model_name):
     '''
     produces classification report for training and testing results and stores report as image
     in images folder
@@ -127,23 +129,34 @@ def classification_report_image(y_train, y_test, y_train_preds, y_test_preds, mo
             y_train_preds_rf: training predictions from random forest
             y_test_preds_lr: test predictions from logistic regression
             y_test_preds_rf: test predictions from random forest
+            model_name: (str) string of file name
 
     output:
              None
     '''
     file_name = model_name + ".png"
     plt.rc('figure', figsize=(5, 5))
-    
+
     # train data report
-    plt.text(0.01, 1.25, str(model_name + ' Train'), {'fontsize': 10}, fontproperties='monospace')
-    plt.text(0.01, 0.05, str(classification_report(y_train, y_train_preds)), {'fontsize': 10}, fontproperties='monospace')
+    plt.text(0.01, 1.25, str(model_name + ' Train'),
+             {'fontsize': 10}, fontproperties='monospace')
+    plt.text(
+        0.01, 0.05, str(
+            classification_report(
+                y_train, y_train_preds)), {
+            'fontsize': 10}, fontproperties='monospace')
 
     # test data report
-    plt.text(0.01, 0.6, str(model_name + ' Test'), {'fontsize': 10}, fontproperties='monospace')
-    plt.text(0.01, 0.7, str(classification_report(y_test, y_test_preds)), {'fontsize': 10}, fontproperties='monospace')
-    
+    plt.text(0.01, 0.6, str(model_name + ' Test'),
+             {'fontsize': 10}, fontproperties='monospace')
+    plt.text(
+        0.01, 0.7, str(
+            classification_report(
+                y_test, y_test_preds)), {
+            'fontsize': 10}, fontproperties='monospace')
+
     plt.axis('off')
-    plt.savefig(output_pth + file_name)
+    plt.savefig("./images/results/" + file_name)
     plt.close(file_name)
     plt.clf()
 
@@ -174,6 +187,7 @@ def feature_importance_plot(model, X_data, output_pth):
     plt.close(file_name)
     plt.clf()
 
+
 def train_models(X_train, X_test, y_train, y_test):
     '''
     train, store model results: images + scores, and store models
@@ -190,8 +204,8 @@ def train_models(X_train, X_test, y_train, y_test):
     param_grid = {
         'n_estimators': [200, 500],
         'max_features': ['auto', 'sqrt'],
-        'max_depth' : [4,5,100],
-        'criterion' :['gini', 'entropy']
+        'max_depth': [4, 5, 100],
+        'criterion': ['gini', 'entropy']
     }
     cv_rfc = GridSearchCV(estimator=rfc, param_grid=param_grid)
     cv_rfc.fit(X_train, y_train)
@@ -209,51 +223,71 @@ def train_models(X_train, X_test, y_train, y_test):
 
     # plot the roc curve
     plt.figure(figsize=(15, 8))
-    ax = plt.gca()
+    axis = plt.gca()
     lrc_plot = plot_roc_curve(lrc, X_test, y_test)
-    rfc_disp = plot_roc_curve(cv_rfc.best_estimator_, X_test, y_test, ax=ax, alpha=0.8)
-    lrc_plot.plot(ax=ax, alpha=0.8)
+    rfc_disp = plot_roc_curve(
+        cv_rfc.best_estimator_,
+        X_test,
+        y_test,
+        ax=axis,
+        alpha=0.8)
+    lrc_plot.plot(ax=axis, alpha=0.8)
+    rfc_disp.plot(ax=axis, alpha=0.8)
     plt.savefig('./images/results/roc_curve.png')
     plt.close('roc_curve.png')
 
     # feature_importance_plot
-    feature_importance_plot(cv_rfc.best_estimator_, X_train, "./images/results/")
+    feature_importance_plot(
+        cv_rfc.best_estimator_,
+        X_train,
+        "./images/results/")
 
     # classification_report image
-    classification_report_image(y_train, y_test, y_train_preds_rf, y_test_preds_rf, "Random_Forest", "./images/results/")
-    classification_report_image(y_train, y_test, y_train_preds_lr, y_test_preds_lr, "Logistic_Regression", "./images/results/")
+    classification_report_image(
+        y_train,
+        y_test,
+        y_train_preds_rf,
+        y_test_preds_rf,
+        "Random_Forest")
+    classification_report_image(
+        y_train,
+        y_test,
+        y_train_preds_lr,
+        y_test_preds_lr,
+        "Logistic_Regression")
+
 
 if __name__ == "__main__":
-        cat_columns = [
-                'Gender',
-                'Education_Level',
-                'Marital_Status',
-                'Income_Category',
-                'Card_Category'
-                ]
-        column_post_fix = '_Churn'
-        keep_cols = [
-                'Customer_Age',
-                'Dependent_count',
-                'Months_on_book',
-                'Total_Relationship_Count', 'Months_Inactive_12_mon',
-                'Contacts_Count_12_mon',
-                'Credit_Limit',
-                'Total_Revolving_Bal',
-                'Avg_Open_To_Buy',
-                'Total_Amt_Chng_Q4_Q1',
-                'Total_Trans_Amt',
-                'Total_Trans_Ct',
-                'Total_Ct_Chng_Q4_Q1',
-                'Avg_Utilization_Ratio',
-                'Gender_Churn',
-                'Education_Level_Churn',
-                'Marital_Status_Churn',
-                'Income_Category_Churn',
-                'Card_Category_Churn'
-                ]
-        df = import_data("./data/bank_data.csv")
-        perform_eda(df)
-        df = encoder_helper(df, cat_columns, column_post_fix)
-        X_train, X_test, y_train, y_test = perform_feature_engineering(df, keep_cols, 'Churn')
-        train_models(X_train, X_test, y_train, y_test)
+    CAT_COLUMNS = [
+        'Gender',
+        'Education_Level',
+        'Marital_Status',
+        'Income_Category',
+        'Card_Category'
+    ]
+    KEEP_COLS = [
+        'Customer_Age',
+        'Dependent_count',
+        'Months_on_book',
+        'Total_Relationship_Count', 'Months_Inactive_12_mon',
+        'Contacts_Count_12_mon',
+        'Credit_Limit',
+        'Total_Revolving_Bal',
+        'Avg_Open_To_Buy',
+        'Total_Amt_Chng_Q4_Q1',
+        'Total_Trans_Amt',
+        'Total_Trans_Ct',
+        'Total_Ct_Chng_Q4_Q1',
+        'Avg_Utilization_Ratio',
+        'Gender_Churn',
+        'Education_Level_Churn',
+        'Marital_Status_Churn',
+        'Income_Category_Churn',
+        'Card_Category_Churn'
+    ]
+    df = import_data("./data/bank_data.csv")
+    perform_eda(df)
+    df = encoder_helper(df, CAT_COLUMNS, '_Churn')
+    X_TRAIN, X_TEST, Y_TRAIN, Y_TEST = perform_feature_engineering(
+        df, KEEP_COLS, 'Churn')
+    train_models(X_TRAIN, X_TEST, Y_TRAIN, Y_TEST)
--- churn_script_logging_and_tests.py
+++ churn_script_logging_and_tests.py
@@ -5,125 +5,135 @@ import churn_library as clib
 
 logging.basicConfig(
     filename='./logs/churn_library.log',
-    level = logging.INFO,
+    level=logging.INFO,
     filemode='w',
     format='%(name)s - %(levelname)s - %(message)s')
 
+
 def test_import():
-	'''
-	test data import - function to test perform_eda() function
-	'''
-	try:
-		df = clib.import_data("./data/bank_data.csv")
-		logging.info("Testing import_data: SUCCESS")
-	except FileNotFoundError as err:
-		logging.error("Testing import_eda: The file wasn't found")
-		raise err
+    '''
+    test data import - function to test perform_eda() function
+    '''
+    try:
+        df = clib.import_data("./data/bank_data.csv")
+        logging.info("Testing import_data: SUCCESS")
+    except FileNotFoundError as err:
+        logging.error("Testing import_eda: The file wasn't found")
+        raise err
 
-	try:
-		assert df.shape[0] > 0
-		assert df.shape[1] > 0
-		return df
-	except AssertionError as err:
-		logging.error("Testing import_data: The file doesn't appear to have rows and columns")
-		raise err
-
-
-def test_eda(df):
-	'''
-	test perform eda function - function to test import_data() function
-	'''
-	try:
-		clib.perform_eda(df)
-		assert os.path.exists('./images/eda/churn_hist.png')
-		assert os.path.exists('./images/eda/Customer_Age.png')
-		assert os.path.exists('./images/eda/Marital_Status.png')
-		assert os.path.exists('./images/eda/Total_Trans_Ct.png')
-		assert os.path.exists('./images/eda/heatmap.png')
-		logging.info("Testing perform_eda: SUCCESS")
-	except AssertionError as err:
-		logging.error("Testing perform_eda: image files don't appear to be generated")
-		raise err
+    try:
+        assert df.shape[0] > 0
+        assert df.shape[1] > 0
+        return df
+    except AssertionError as err:
+        logging.error(
+            "Testing import_data: The file doesn't appear to have rows and columns")
+        raise err
 
 
+def test_eda(dataframe):
+    '''
+    test perform eda function - function to test import_data() function
+    '''
+    try:
+        clib.perform_eda(dataframe)
+        assert os.path.exists('./images/eda/churn_hist.png')
+        assert os.path.exists('./images/eda/Customer_Age.png')
+        assert os.path.exists('./images/eda/Marital_Status.png')
+        assert os.path.exists('./images/eda/Total_Trans_Ct.png')
+        assert os.path.exists('./images/eda/heatmap.png')
+        logging.info("Testing perform_eda: SUCCESS")
+    except AssertionError as err:
+        logging.error(
+            "Testing perform_eda: image files don't appear to be generated")
+        raise err
 
-def test_encoder_helper(df, category_lst, response):
-	'''
-	test encoder helper - function to test encoder_helper() function
-	'''
-	try:
-		encoded_df = clib.encoder_helper(df, category_lst, response)
-		original_df = clib.import_data("./data/bank_data.csv")
-		assert (len(encoded_df.columns) == (len(original_df.columns) + len(category_lst) + 1))
-		logging.info("Testing encoder_helper: SUCCESS")
-	except AssertionError as err:
-		logging.error("Testing encoder_helper: The dataframe doesn't appear to have additional encoded columns")
-		raise err
 
-def test_perform_feature_engineering(df, cols, response):
-	'''
-	test perform_feature_engineering - function to test perform_feature_engineering() function
-	'''
-	try:
-		X_train, X_test, y_train, y_test = clib.perform_feature_engineering(df, cols, response)
-		assert X_train.shape[0] > 0
-		assert X_test.shape[0] > 0
-		assert y_train.shape[0] > 0
-		assert y_test.shape[0] > 0
-		logging.info("Testing perform_feature_engineering: SUCCESS")
-		return X_train, X_test, y_train, y_test
-	except AssertionError as err:
-		logging.error("Testing perform_feature_engineering: The dataframes appear to be empty")
-		raise err
+def test_encoder_helper(dataframe, category_lst, response):
+    '''
+    test encoder helper - function to test encoder_helper() function
+    '''
+    try:
+        encoded_dataframe = clib.encoder_helper(dataframe, category_lst, response)
+        original_dataframe = clib.import_data("./data/bank_data.csv")
+        assert (len(encoded_dataframe.columns) == (
+            len(original_dataframe.columns) + len(category_lst) + 1))
+        logging.info("Testing encoder_helper: SUCCESS")
+    except AssertionError as err:
+        logging.error(
+            "Testing encoder_helper: The dataframe doesn't appear to have additional encoded columns")
+        raise err
+
+
+def test_perform_feature_engineering(dataframe, cols, response):
+    '''
+    test perform_feature_engineering - function to test perform_feature_engineering() function
+    '''
+    try:
+        X_train, X_test, y_train, y_test = clib.perform_feature_engineering(
+            dataframe, cols, response)
+        assert X_train.shape[0] > 0
+        assert X_test.shape[0] > 0
+        assert y_train.shape[0] > 0
+        assert y_test.shape[0] > 0
+        logging.info("Testing perform_feature_engineering: SUCCESS")
+        return X_train, X_test, y_train, y_test
+    except AssertionError as err:
+        logging.error(
+            "Testing perform_feature_engineering: The dataframes appear to be empty")
+        raise err
 
 
 def test_train_models(X_train, X_test, y_train, y_test):
-	'''
-	test train_models - function to test train_models() function
-	'''
-	try:
-		clib.train_models(X_train, X_test, y_train, y_test)
-		assert os.path.exists('./images/results/roc_curve.png')
-		assert os.path.exists('./images/results/feature_importance.png')
-		assert os.path.exists('./images/results/Random_Forest.png')
-		assert os.path.exists('./images/results/Logistic_Regression.png')
-		assert os.path.exists('./models/rfc_model.pkl')
-		assert os.path.exists('./models/logistic_model.pkl')
-		logging.info("Testing train_models: SUCCESS")
-	except AssertionError as err:
-		logging.error("Testing train_models: model and/or image files don't appear to be generated")
-		raise err
+    '''
+    test train_models - function to test train_models() function
+    '''
+    try:
+        clib.train_models(X_train, X_test, y_train, y_test)
+        assert os.path.exists('./images/results/roc_curve.png')
+        assert os.path.exists('./images/results/feature_importance.png')
+        assert os.path.exists('./images/results/Random_Forest.png')
+        assert os.path.exists('./images/results/Logistic_Regression.png')
+        assert os.path.exists('./models/rfc_model.pkl')
+        assert os.path.exists('./models/logistic_model.pkl')
+        logging.info("Testing train_models: SUCCESS")
+    except AssertionError as err:
+        logging.error(
+            "Testing train_models: model and/or image files don't appear to be generated")
+        raise err
+
 
 if __name__ == "__main__":
-	cat_columns = [
-		'Gender',
-		'Education_Level',
-		'Marital_Status',
-		'Income_Category',
-		'Card_Category'
-                ]
-	keep_cols = [
-		'Customer_Age',
-		'Dependent_count',
-		'Months_on_book',
-		'Total_Relationship_Count', 'Months_Inactive_12_mon',
-		'Contacts_Count_12_mon',
-		'Credit_Limit',
-		'Total_Revolving_Bal',
-		'Avg_Open_To_Buy',
-		'Total_Amt_Chng_Q4_Q1',
-		'Total_Trans_Amt',
-		'Total_Trans_Ct',
-		'Total_Ct_Chng_Q4_Q1',
-		'Avg_Utilization_Ratio',
-		'Gender_Churn',
-		'Education_Level_Churn',
-		'Marital_Status_Churn',
-		'Income_Category_Churn',
-		'Card_Category_Churn'
-		]
-	df = test_import()
-	test_eda(df)
-	test_encoder_helper(df, cat_columns, '_Churn')
-	X_train, X_test, y_train, y_test = test_perform_feature_engineering(df, keep_cols, 'Churn')
-	test_train_models(X_train, X_test, y_train, y_test)
+    cat_columns = [
+        'Gender',
+        'Education_Level',
+        'Marital_Status',
+        'Income_Category',
+        'Card_Category'
+    ]
+    keep_cols = [
+        'Customer_Age',
+        'Dependent_count',
+        'Months_on_book',
+        'Total_Relationship_Count', 'Months_Inactive_12_mon',
+        'Contacts_Count_12_mon',
+        'Credit_Limit',
+        'Total_Revolving_Bal',
+        'Avg_Open_To_Buy',
+        'Total_Amt_Chng_Q4_Q1',
+        'Total_Trans_Amt',
+        'Total_Trans_Ct',
+        'Total_Ct_Chng_Q4_Q1',
+        'Avg_Utilization_Ratio',
+        'Gender_Churn',
+        'Education_Level_Churn',
+        'Marital_Status_Churn',
+        'Income_Category_Churn',
+        'Card_Category_Churn'
+    ]
+    df = test_import()
+    test_eda(df)
+    test_encoder_helper(df, cat_columns, '_Churn')
+    X_TRAIN, X_TEST, Y_TRAIN, Y_TEST = test_perform_feature_engineering(
+        df, keep_cols, 'Churn')
+    test_train_models(X_TRAIN, X_TEST, Y_TRAIN, Y_TEST)
